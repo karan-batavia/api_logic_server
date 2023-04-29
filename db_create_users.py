7694b94f5bcbd675ec9828aee230cea881313864
Karan <karan.batavia@privado.ai>
2023-04-29 14:36:08 +0530

    sqlalchemy test

--- db_create_users.py
+++ db_create_users.py
@@ -1,10 +1,16 @@
 from project import db
 from project.models import User
+from project.model2 import Specie
+
 
 # insert data
 db.session.add(User("michael", "michael@realpython.com", "i'll-never-tell"))
 db.session.add(User("admin", "ad@min.com", "admin"))
 db.session.add(User("mike", "mike@herman.com", "tell"))
+
+Specie.query.all()
+
+Specie.query.add_columns()
 
 # commit the changes
 db.session.commit()
